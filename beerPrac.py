8831a49fe42757366c043051aaece68d04bf5a83
Mike Turke <turkem1993@gmail.com>
2018-12-18 19:13:23 -0500

    Update beerPrac.py

--- beerPrac.py
+++ beerPrac.py
@@ -1,1 +1,16 @@
-x = 99   while x > 1:    print(x, "bottles of beer on the wall,", x,"bottles of beer,")    x -= 1    print("take one down, pass it around,", x, "bottles of beer on the wall.")        print("")if x == 1:    print(x, "bottle of beer on the wall,", x, "bottle of beer,")    print("take it down, pass it around, no more bottles of beer on the wall.")    print(" ")    print("No more bottles of beer on the wall, no more bottles of beer..")
+#  Simple "99 bottles of beer on the wall" program using while loop and if statement.
+x = 99   
+
+while x > 1:
+
+    print(x, "bottles of beer on the wall,", x,"bottles of beer,")
+    x -= 1
+    print("take one down, pass it around,", x, "bottles of beer on the wall.")
+    
+    print("")
+
+if x == 1:
+    print(x, "bottle of beer on the wall,", x, "bottle of beer,")
+    print("take it down, pass it around, no more bottles of beer on the wall.")
+    print(" ")
+    print("No more bottles of beer on the wall, no more bottles of beer..")
